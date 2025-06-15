8f6c379fe1168851598cdd03e328e6290e4a5f77
freesia <freesia>
2025-06-16 00:52:56 +0800

    modified:   segquant/__init__.py

--- segquant/__init__.py
+++ segquant/__init__.py
@@ -1,6 +1,6 @@
 from .utils.extension import load_fake_quant_fp8_ext, load_real_quant_fp8_ext, load_real_quant_int8_ext, load_real_quant_int4_ext, load_real_quant_mix_ext
 
-# load_fake_quant_fp8_ext(verbose=True, required=True)
+load_fake_quant_fp8_ext(verbose=True, required=True)
 load_real_quant_fp8_ext(verbose=True, required=True)
 load_real_quant_int8_ext(verbose=True, required=True)
 load_real_quant_int4_ext(verbose=True, required=True)
