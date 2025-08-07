2281175bdfbba92f2912a1c0494dedb194ec687c
freesia <freesia>
2025-08-07 16:46:47 +0800

    modified:   test/test_unfold.py

--- test/test_unfold.py
+++ test/test_unfold.py
@@ -1,3 +1,4 @@
+import math
 import pandas as pd
 import torch
 import torch.nn.functional as F
@@ -13,10 +14,33 @@ def im2col(X, kernel_size, stride=1, padding=0):
     X_col = F.unfold(X, kernel_size=kernel_size, dilation=1, padding=padding, stride=stride)
     return X_col
 
+def shift_tensor_columns(tensor, k, r, stride):
+    HxW, total_cols = tensor.shape
+    num_groups = math.ceil(total_cols / (k * stride))
+    result = torch.empty_like(tensor)
+
+    for i in range(num_groups):
+        start = i * (k * stride)
+        end = (i + 1) * (k * stride)
+        shift_rows = (i * r) % HxW
+
+        if end > total_cols:
+            end = total_cols
+
+        print(f"Processing group {i}: start={start}, end={end}, total_cols={total_cols}, shift_rows={shift_rows}")
+        segment = tensor[:, start:end]
+        shifted = torch.roll(segment, shifts=shift_rows, dims=0)
+        result[:, start:end] = shifted
+
+    return result
+
+def conv_output_size(input_size, kernel_size, stride=1, padding=0, dilation=1):
+    return (input_size + 2 * padding - dilation * (kernel_size - 1) - 1) // stride + 1
+
 if __name__ == "__main__":
-    B, C_in, H, W = 1, 3, 9, 9
-    Kh, Kw = 3, 3
-    stride = 1
+    B, C_in, H, W = 1, 3, 7, 7
+    Kh, Kw = 5, 5
+    stride = 2
     padding = 1
     
     X = torch.arange(start=1, end=B*C_in*H*W+1).float().reshape(B, C_in, H, W)
@@ -38,3 +62,11 @@ if __name__ == "__main__":
 
     df = pd.DataFrame(X_col.to(dtype=torch.int32).numpy())
     df.to_csv("X_col.csv")
+
+    r = conv_output_size(H, Kw, stride=stride, padding=padding)
+    print(r)
+
+    X_col_shifted = shift_tensor_columns(X_col, Kw, r, stride)
+    df = pd.DataFrame(X_col_shifted.to(dtype=torch.int32).numpy())
+    df.to_csv("X_col_shifted.csv")
+
