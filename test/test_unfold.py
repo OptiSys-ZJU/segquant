051df8043afddef0f2de4bfe901c77da5a668724
freesia <freesia>
2025-08-08 10:07:18 +0800

    modified:   test/test_unfold.py

--- test/test_unfold.py
+++ test/test_unfold.py
@@ -39,24 +39,13 @@ def conv_output_size(input_size, kernel_size, stride=1, padding=0, dilation=1):
 
 if __name__ == "__main__":
     B, C_in, H, W = 1, 3, 7, 7
-    Kh, Kw = 5, 5
-    stride = 2
+    Kh, Kw = 3, 3
+    stride = 1
     padding = 1
     
     X = torch.arange(start=1, end=B*C_in*H*W+1).float().reshape(B, C_in, H, W)
-
-    # df = pd.DataFrame(X[0][0].to(dtype=torch.int32).numpy())
-    # df.to_csv("X_0.csv")
-
-    # df = pd.DataFrame(X[0][1].to(dtype=torch.int32).numpy())
-    # df.to_csv("X_1.csv")
-
-    # df = pd.DataFrame(X[0][2].to(dtype=torch.int32).numpy())
-    # df.to_csv("X_2.csv")
     
     X_col = im2col(X, kernel_size=(Kh, Kw), stride=stride, padding=padding)[0].T
-    # print(f"X_col shape: {X_col.shape}")
-    # print(f"X_col: {X_col}")
     X_col = X_col[:, 0:Kh*Kw]
     print(X_col)
 
